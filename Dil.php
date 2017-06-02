c84d4168fc2b1188e661df4fb3b190af79883a6a
Sevenops <we.are.dev.tr@gmail.com>
2017-06-02 15:27:50 +0300

    Create Dil.php
    Language system for xenforo forum

---
+++ Dil.php
@@ -0,0 +1,52 @@
+
+<?php
+$arr = array(
+	'es-ES' => array(
+		"Communities" => 'Comunidades',
+		'International' => 'Internacional',
+		'Announcements' => 'Anuncios',
+		'Map Submissions' => 'Presentación de mapas',
+		'English' => 'Inglesa',
+		'French' => 'Francesa',
+		'Brazilian' => 'Brasileña',
+		'Spanish' => 'Española',
+		'Czech' => 'Checa',
+		'Turkish' => 'Turca',
+		'Polish' => 'Polaca',
+		'Hungarian' => 'Húngara',
+		'Romanian' => 'Rumana',
+		'Arabic' => 'Árabe',
+		'Russian' => 'Rusa',
+		'Filipino' => 'Filipina',
+		'Forums' => 'Foros',
+		'Discussions' => 'Discusiones',
+		'Off Topic' => 'Off Topic',
+		'Modules' => 'Modules',
+		'Tribes' => 'Tribus',
+		'Forum Games' => 'Juegos del Foro'
+	) ,
+	'tr-TR' => array(
+		'English' => 'İngilizce',
+		"Communities" => 'Topluluklar',
+		'International' => 'Uluslararası',
+		'Announcements' => 'Duyurular',
+		'Map Submissions' => 'Harita Yollama',
+		'French' => 'Fransızca',
+		'Brazilian' => 'Brezilya',
+		'Spanish' => 'İspanyolca',
+		'Turkish' => 'Türkçe',
+		'Polish' => 'Lehçe',
+		'Hungarian' => 'Macarca',
+		'Romanian' => 'Romence',
+		'Arabic' => 'Arapça',
+		'Russian' => 'Rusça',
+		'Filipino' => 'Filipince',
+		'Forums' => 'Forumlar',
+		'Discussions' => 'Tartışmalar',
+		'Off Topic' => 'Konu Dışı',
+		'Modules' => 'Modüller',
+		'Tribes' => 'Kabileler',
+		'Forum Games' => 'Forum Oyunları',
+		'Czech' => 'Çekce'
+	)
+);
